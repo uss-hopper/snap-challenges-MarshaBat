3f4d0bb8fcdc71844d2b6b71d6ae89208642e366
Marsha Battee <marsha@sanesuite.com>
2019-07-11 08:49:52 -0600

    user story page created

---
+++ snap_7_9/script.js
@@ -0,0 +1,34 @@
+/**SNAP CHALLENGE - TUESDAY - July, 9, 2019
+ *
+ * FUNCTION #1: Take two numbers as arguments and return a new value after a mathematical operation
+(addition, subtraction, multiplication, etc.)  Which operation is your choice.
+ **/
+
+function dog (num1, num2) {
+	return num1 / num2;
+};
+
+let opera = dog(3,6);
+console.log(opera);
+
+console.log(dog(3,6));
+
+console.log(dog(7,3));
+console.log(dog(3,7));
+console.log(dog(750,3));
+
+let punk = dog(7,3);
+let jazz = dog(3,7);
+let ska = dog(750,3);
+
+console.log(punk, ska, jazz);
+
+//FUNCTION #2 Take two strings as arguments and return a value that has them concatenated.
+
+
+function concatenateTwoStrings (string1, string2) {
+	return string1 + " " + string2;
+}
+console.log(concatenateTwoStrings('thingOne', 'thingTwo'));
+
+/*************END OF SNAP CHALLENGE - TUESDAY - July, 9, 2019*********/
