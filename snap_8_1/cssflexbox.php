98e32c7d6e4a7829a7b6c8431d8f7e5743fd1d51
Marsha Battee <marsha@sanesuite.com>
2019-08-01 08:41:26 -0600

    added flexbox container to list

--- snap_8_1/cssflexbox.php
+++ snap_8_1/cssflexbox.php
@@ -1,11 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
 
+		<!-- custom CSS stylesheet -->
+		<link rel="stylesheet" href="style.css" type="text/css" />
+		<title>CSS Mockup Activity</title>
+	</head>
 
+	<body>
+		<p>What is the total width and height of the element in pixels on the screen for the above box model?</p>
+		<p>Height = 170 px / Width = 210 px</p>
 
-<ul class="my-list">
-	<li class="my-item">1</li>
-	<li class="my-item">2</li>
-	<li class="my-item">3</li>
-	<li class="my-item">4</li>
-	<li class="my-item">5</li>
-	<li class="my-item">6</li>
-</ul>
+			<ul class="my-list">
+				<li class="my-item">1</li>
+				<li class="my-item">2</li>
+				<li class="my-item">3</li>
+				<li class="my-item">4</li>
+				<li class="my-item">5</li>
+				<li class="my-item">6</li>
+			</ul>
+
+	</body>
