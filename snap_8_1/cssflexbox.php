1f80b7cedc5bd605f7962ebf723d18148de964c8
Marsha Battee <marsha@sanesuite.com>
2019-08-01 08:11:02 -0600

    added snap challenge php and css file with content

---
+++ snap_8_1/cssflexbox.php
@@ -0,0 +1,11 @@
+
+
+
+<ul class="my-list">
+	<li class="my-item">1</li>
+	<li class="my-item">2</li>
+	<li class="my-item">3</li>
+	<li class="my-item">4</li>
+	<li class="my-item">5</li>
+	<li class="my-item">6</li>
+</ul>
